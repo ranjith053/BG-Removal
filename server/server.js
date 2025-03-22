7e698f9beeb2288b88700290694149aff6df4d31
RyanRanjith <kingranjith4@gmail.com>
2025-03-21 23:52:19 -0700

    march 21

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import connectDB from './configs/mongodb.js'
 
 // App Config 
 const PORT = process.env.PORT || 4000
 const app = express()
+await connectDB()
 
 // Intialize middlewares
 app.use(express.json())
